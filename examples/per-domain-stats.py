b821f0cc0b257759ef4bd5ca1094ad45b7d630ca
jdelalande <jocelyn.delalande@oasiswork.fr>
2013-09-25 16:00:07 +0200

    Added POC to count accounts per-client. fixes #311, refs #301

---
+++ examples/per-domain-stats.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Example tool to grab per-domains stats.
+#
+
+import argparse
+import getpass
+
+import sys; import os; sys.path.append(os.path.dirname(__file__)+'/../')
+
+from pysimplesoap.client import SoapFault
+from urllib2 import URLError
+
+import zimsoap.client
+from zimsoap.zobjects import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=\
+      'Counts Zimbra accounts, globally or for a subset of domains, sorted by cos.')
+    parser.add_argument("-u", "--username", required=True,
+                        help="zimbra admin username (user@domain.tld)")
+    parser.add_argument("-s", "--server",required=True,
+                        help="zimbra server host or proxy")
+
+    parser.add_argument("-p", "--port", default=7071,
+                        help="server or proxy port (default : 7071)")
+    parser.add_argument("--domain", '-d', action="append", nargs="*",
+                        help="restrict the stats to this domain")
+    args = parser.parse_args()
+
+    if args.domain:
+        # Flattens a list of lists.
+        args.domains_list = [i for sublist in args.domain for i in sublist]
+
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    password = getpass.getpass('Password for %s: ' % args.username)
+
+    zc = zimsoap.client.ZimbraAdminClient(args.server)
+    try:
+        zc.login(args.username, password)
+    except (SoapFault, URLError) as sf:
+        print sf
+        exit(5)
+
+    if args.domain:
+        domains_to_inspect = [Domain(name=i) for i in args.domains_list]
+    else:
+        domains_to_inspect = zc.get_all_domains()
+
+    total_accounts = 0
+
+    print "\nPrint accounts count, per-COS:"
+    for domain in domains_to_inspect:
+        print
+        print "Domain %s" % domain.name
+        for cos , count in zc.count_account(domain):
+            print '{:.<20}{}'.format(cos.name, count)
+            total_accounts += count
+
+print '\nTOTAL ACCOUNTS ({} domains): {}'.format(
+    len(domains_to_inspect), total_accounts)
