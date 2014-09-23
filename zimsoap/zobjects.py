70ba8391551e41399744a52dd046a8ca2d829f43
Jocelyn Delalande <jocelyn.delalande@oasiswork.fr>
2014-09-23 10:28:07 +0200

    mimics dict.get() function to get attributes providing a default val

--- zimsoap/zobjects.py
+++ zimsoap/zobjects.py
@@ -84,6 +84,14 @@ class ZObject(object):
         """
         return self._a_tags[k]
 
+    def get(self, k, default=None):
+        """ Mimics dict() get method
+        """
+        try:
+            return self[k]
+        except KeyError:
+            return default
+
     def __setitem__(self, k, v):
         self._a_tags[k] = v
 
