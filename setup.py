086d35fa882ff82277aaad7684e7cb64e0109f2d
Jocelyn Delalande <jdelalande@oasiswork.fr>
2014-10-15 17:04:47 +0200

    removed git stuff from version numbers

--- setup.py
+++ setup.py
@@ -10,21 +10,8 @@ except IOError:
     README = ''
 
 
-def mk_version(base_version):
-    try:
-        try:
-            GIT_HEAD = open('.git/HEAD').readline().split(':')[1].strip()
-        except IndexError: # if we checkout a commit
-            GIT_HEAD_REV = open('.git/HEAD').readline()
-        else:
-            GIT_HEAD_REV = open('.git/{0}'.format(GIT_HEAD)).readline().strip()
-
-        return '{0}-git-{1:.7}'.format(base_version, GIT_HEAD_REV)
-    except IOError:
-        return base_version
-
 setup(name='zimsoap',
-      version=mk_version('0.2.3'),
+      version='0.2.3',
       description='A high-level library to access programaticaly Zimbra \
                    SOAP API features',
       long_description=README,
