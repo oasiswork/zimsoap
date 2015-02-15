8f85c713b46963811e2349647f8fe7ac2d0c2b21
Florent Manens <florent@manens.org>
2015-02-15 15:48:18 +0100

    Fix REST access for missing variables (isadmin and TOKEN_COOKIE)

--- zimsoap/client.py
+++ zimsoap/client.py
@@ -29,6 +29,8 @@ class RESTClient:
     """ Abstract Classe, RESTClient defines a REST client for some operations we
     can't do with SOAP API, such as admin preauth.
     """
+    TOKEN_COOKIE = "ZM_AUTH_TOKEN"
+
     class NoPreauthKeyProvided(Exception):
         pass
 
@@ -38,13 +40,14 @@ class RESTClient:
             self.msg = 'Zimbra issued HTTP error : '+e.msg
             Exception.__init__(self, self.msg)
 
-    def __init__(self, server_host, server_port=None, preauth_key=None):
+    def __init__(self, server_host, server_port=None, preauth_key=None, isadmin=False):
         if server_port:
             self.preauth_url = 'https://{0}:{1}/service/preauth?'.format(
                 server_host, server_port)
         else:
             self.preauth_url = 'https://{0}/service/preauth?'.format(server_host)
 
+        self.isadmin = isadmin
         self.set_preauth_key(preauth_key)
 
     def set_preauth_key(self, preauth_key):
