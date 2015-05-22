7f2b128b7058610cde197d474b34324bc08fd119
Jocelyn Delalande <jdelalande@oasiswork.fr>
2015-05-22 12:43:21 +0200

    updated patch version to upload to pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except IOError:
 
 
 setup(name='zimsoap',
-      version='0.4',
+      version='0.4.1',
       description='A high-level library to access programaticaly Zimbra \
                    SOAP API features',
       long_description=README,
