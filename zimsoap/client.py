234ded44d09d40d2168ad5bc667666f4cf33984f
Emeric Million <emillion@oasiswork.fr>
2016-02-09 18:31:32 +0100

    Add and remove white/black lists on an account

--- zimsoap/client.py
+++ zimsoap/client.py
@@ -447,6 +447,39 @@ class ZimbraAccountClient(ZimbraAbstractClient):
         """
         self.request('ModifyIdentity', {'identity': identity.to_creator()})
 
+    # Whitelists and Blacklists
+
+    def get_white_black_lists(self):
+        return self.request('GetWhiteBlackList')
+
+    def add_to_blacklist(self, values):
+        param = {'blackList': {'addr': []}}
+        for value in values:
+            param['blackList']['addr'].append({'op': '+', '_content': value})
+
+        self.request('ModifyWhiteBlackList', param)
+
+    def remove_from_blacklist(self, values):
+        param = {'blackList': {'addr': []}}
+        for value in values:
+            param['blackList']['addr'].append({'op': '-', '_content': value})
+
+        self.request('ModifyWhiteBlackList', param)
+
+    def add_to_whitelist(self, values):
+        param = {'whiteList': {'addr': []}}
+        for value in values:
+            param['whiteList']['addr'].append({'op': '+', '_content': value})
+
+        self.request('ModifyWhiteBlackList', param)
+
+    def remove_from_whitelist(self, values):
+        param = {'whiteList': {'addr': []}}
+        for value in values:
+            param['whiteList']['addr'].append({'op': '-', '_content': value})
+
+        self.request('ModifyWhiteBlackList', param)
+
 
 class ZimbraAdminClient(ZimbraAbstractClient):
     """ Specialized Soap client to access zimbraAdmin webservice, handling auth.
